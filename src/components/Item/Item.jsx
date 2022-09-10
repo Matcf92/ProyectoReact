3f7cbce329a03f5a2d799a409dbbb16e2c3ffc97
Mathias Fagundez <mathifs92@gmail.com>
2022-09-10 17:39:24 -0300

    item detail

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,7 +4,7 @@ const Item = ({ producto }) => {
 
         return (
                 <div className='cardContainer'>
-                        <img src={producto.img} alt='no carga o khe' />
+                        <img src={producto.imagen} alt='no carga o khe' />
                         <h4 className='tituloCard'>{producto.nombre}</h4>
                         <p className="precioCard">Precio: {producto.precio}</p>
                         <small>Stock disponible: {producto.stock}</small>
--- src/components/ItemDetail/itemDetail.jsx
+++ src/components/ItemDetail/itemDetail.jsx
@@ -5,7 +5,7 @@ const ItemDetail = ({ item }) => {
 
     return (
         <div className="cardDetailContainer">
-            <img src={item.img} alt="No carga no" />
+            <img src={item.imagen} alt="No carga no" />
             <h3>{item.nombre}</h3>
             <p>{item.desc}</p>
             <p>{item.category}</p>
